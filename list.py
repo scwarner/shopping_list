4ef33c8ff981751e9c89be9add20b700707c2c39
Sarah <hayes.sc@gmail.com>
2017-12-29 07:56:58 -0500

    Added function to remove items.

--- list.py
+++ list.py
@@ -1,23 +1,29 @@
 # make a list to hold onto items
 shopping_list = []
+
 # show function to print out list
 def show():
     print("Here is your list: ")
     for item in shopping_list:
         print(item)
 
+ # print out instructions on how to use app
 def help():        
-    # print out instructions on how to use app
     print("""
 Enter 'DONE' to stop adding items.
 Enter 'HELP' to see instructions.
 Enter 'SHOW' to see current list.
+Enter 'REMOVE' to delete last item from list.
 """)
 
+# add new items
 def add_to_list(new_item):
-    # add new items
     shopping_list.append(new_item)
     print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
+
+def remove_item():
+    print("Say 'goodbye' to {}.".format(shopping_list[-1]))
+    shopping_list.pop(-1)
 
 print("What should we pick up at the store?")
 help()
@@ -36,7 +42,10 @@ while True:
         continue
     elif new_item[0] == "s":
         print("Sorry, you can't have that.")
-        continue    
+        continue  
+    elif new_item == 'REMOVE':
+        remove_item()
+        continue     
     add_to_list(new_item)
 
 show()
